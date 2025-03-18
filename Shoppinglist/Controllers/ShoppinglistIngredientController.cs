36c29a64afffd77ba177d5d6eba6d590ebf8c888
Gilles Lagrilliere <gilles.lagrilliere@gmail.com>
2025-03-18 16:52:49 +0100

    update shoppinglistIngredient feature

---
+++ Shoppinglist/Controllers/ShoppinglistIngredientController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RecipeShoppinglist.DTOs;
+using RecipeShoppingList.Repostories;
+
+namespace RecipeShoppinglist.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class ShoppinglistIngredientController : Controller
+    {
+        private readonly ILogger<ShoppinglistIngredientController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
+        public ShoppinglistIngredientController(ILogger<ShoppinglistIngredientController> logger, IUnitOfWork unitOfWork)
+        {
+            _logger = logger;
+            _unitOfWork = unitOfWork;
+        }
+
+        [HttpPut]
+        [Route("{id}")]
+        public ActionResult Update(int id, [FromBody]UpdateShoppinglistIngredientDto ingredientDto)
+        {
+            var ingredient = _unitOfWork.ShoppinglistIngredientRepo.GetById(id);
+
+            if (ingredient == null)
+            {
+                return NotFound();
+            }
+
+            ingredient.Measurement = ingredientDto.Measurement;
+            ingredient.IsChecked = ingredientDto.IsChecked;
+            ingredient.Quantity = ingredientDto.Quantity;
+
+            _unitOfWork.ShoppinglistIngredientRepo.Update(ingredient);
+            _unitOfWork.Save();
+
+            return Ok();
+        }
+    }
+}
